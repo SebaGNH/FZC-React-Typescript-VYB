d0f54c1831bd0705db663d67a6a8d43161a7bf8e
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-09-13 19:04:30 -0300

    se agregan elementos como lista con estilos de bootstrap

--- src/fz01-task-app/components/FormTask.tsx
+++ src/fz01-task-app/components/FormTask.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ButtonBack } from "../../components/ButtonBack";
 
 // Lo creamos con un Type para que sea más abreviado
@@ -13,27 +13,46 @@ export const FormTask = () => {
   // const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) : void => {
   const handleSubmit = (ev: FormElement) : void => {
     ev.preventDefault();
-    console.log('Submit:', newTask);
     setTaskList([...taskList, newTask]);
-    setNewTask('')
+    setNewTask(''); // limpiamos
   }
+
+  const removeTask = (i: number) => {
+    const updateTasks = [...taskList];
+    updateTasks.splice(i,1);
+    setTaskList(updateTasks);
+  }
+
   return (
     <div className='container'>
       <ButtonBack />
 
-      <h1>Formulario</h1>
+      <h1>Lista de tareas con formulario simple</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="col-md-6">
         <input
           type="text"
+          value={newTask}
           onChange={ ev => setNewTask( ev.target.value)}
+          className="col-md-12"
         />
         <br />
-        <button className="btn btn-primary mt-2">Save</button>
+        <button className="btn btn-primary mt-2 col-md-12">Save</button>
       </form>
+
+      <div className="mt-3 d-flex flex-column col-md-3">
       {
-        taskList.map( (t: string, i: number) => <p key={i}>{t}</p>)
+        taskList.map( (t: string, i: number) =>
+          (
+            <div
+              key={i}
+              onClick={() => removeTask(i)}
+              className="btn btn-outline-secondary p-2 my-2"
+            >{t}</div>
+          )
+        )
       }
+      </div>
     </div>
   )
 }
