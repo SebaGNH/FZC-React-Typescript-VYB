0d6ed9bbee522ffd842f4f12a10a0e7fcc2f3c26
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-10-02 19:27:20 -0300

    se agrega evento incrementar

--- src/cs01-useReducer/pages/CSCounterReducer.tsx
+++ src/cs01-useReducer/pages/CSCounterReducer.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useReducer } from 'react';
 import { ButtonBack } from '../../components/ButtonBack'
 
+interface StateI {
+  count: number;
+  error: string | null;
+}
+
+interface ActionI {
+  type: 'increment' | 'decrement';
+}
+
+const initialState: StateI = {
+  count: 0,
+  error: null
+}
+
+const reducer = (state: StateI = initialState, action: ActionI) => {
+  const { type } = action;
+  switch (type) {
+    case 'increment': return (
+      state = { count: state.count +1 , error: null}
+    )
+
+    default:
+      return state;
+  }
+
+}
+
+
+
 export const CSCounterReducer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // onClick
+  const handeClick =  () => {
+    dispatch({
+      type: 'increment'
+    });
+  }
+
   return (
     <div className='container'>
       <ButtonBack />
       <h1>Contador Cosden Solution</h1>
+
+      <h2>{state.count}</h2>
+      <p>{state.error}</p>
+
+      <button
+        className='btn btn-primary'
+        onClick={() => handeClick()}
+      >
+        Incrementar
+      </button>
+
     </div>
   )
 }
